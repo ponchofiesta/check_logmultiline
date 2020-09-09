fc9b0439b188a1950d94031373b071cb574ffe91
ponchofiesta <mr@osor.de>
2020-09-09 15:59:07 +0200

    reorganize status code

--- src/main.rs
+++ src/main.rs
@@ -82,46 +82,57 @@ fn main() {
     if let Err(e) = state_loader.save(&statedoc) {
         unknown(&format!("Could not save state file: {}", e));
     };
-    // Check results
-    let mut code = ProblemType::OK;
-    if results.iter().any(|result| {
+
+    // Check results and set status code
+    let is_critical = results.iter().any(|result| {
         result
             .messages
             .iter()
             .any(|message| message.message_type == ProblemType::CRITICAL)
-    }) {
-        code = ProblemType::CRITICAL;
-    } else if results.iter().any(|result| {
+    });
+    let is_warning = results.iter().any(|result| {
         result
             .messages
             .iter()
             .any(|message| message.message_type == ProblemType::WARNING)
-    }) {
-        code = ProblemType::WARNING;
-    }
+    });
+
+    let code = if is_critical {
+        ProblemType::CRITICAL
+    } else if is_warning {
+        ProblemType::WARNING
+    } else {
+        ProblemType::OK
+    };
 
     // Generate output message for results
     let mut msg = String::from(RESULT_NAME);
     msg.push_str(&format!(" {}: ", code));
 
-    msg.push_str(&format!(
-        "{} warnings and {} criticals in {} lines of {} files\n",
-        results.iter().fold(0, |count, matches| count
+    let warnings_count = results.iter().fold(0, |count, matches| {
+        count
             + matches
                 .messages
                 .iter()
                 .filter(|message| message.message_type == ProblemType::WARNING)
-                .count()),
-        results.iter().fold(0, |count, matches| count
+                .count()
+    });
+    let criticals_count = results.iter().fold(0, |count, matches| {
+        count
             + matches
                 .messages
                 .iter()
                 .filter(|message| message.message_type == ProblemType::CRITICAL)
-                .count()),
-        results
-            .iter()
-            .fold(0, |count, matches| count + matches.lines_count),
-        results.iter().len()
+                .count()
+    });
+    let lines_count = results
+        .iter()
+        .fold(0, |count, matches| count + matches.lines_count);
+    let files_count = results.iter().len();
+
+    msg.push_str(&format!(
+        "{} warnings and {} criticals in {} lines of {} files\n",
+        warnings_count, criticals_count, lines_count, files_count
     ));
 
     for matches in results.iter() {
